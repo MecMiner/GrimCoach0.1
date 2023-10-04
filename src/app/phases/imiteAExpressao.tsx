e00274de9bcc424cb7a3623c43eb85e32d803f5c
MecMiner <souza.jefersoncarlos@gmail.com>
2023-10-04 17:56:54 -0300

    'ola'

--- src/app/phases/imiteAExpressao.tsx
+++ src/app/phases/imiteAExpressao.tsx
@@ -10,7 +10,7 @@ import Title from '../components/title';
 import SpeechText from '../components/SpeechText';
 import { StatusBar } from 'expo-status-bar';
 
-interface Respost{
+interface Respost {
   angerLikelihood: string | null,
   joyLikelihood: string | null,
   surpriseLikelihood: string | null,
@@ -21,20 +21,20 @@ interface Respost{
 const { width, height } = Dimensions.get('window');
 
 export default function ImiteAExpressao() {
-  const [type, setType] = useState(CameraType.front);
-  const [permission, requestPermission] = Camera.useCameraPermissions();
-  const [image, setImage] = useState(null);
-  const [imageBase64, setImageBase64] = useState(null);
-  const [openCamera, setOpenCamera] = useState(false);
-  const [respost, setRespost] = useState<Respost | null>();
+  const [permission, requestPermission] = Camera.useCameraPermissions(); //Const permissao para uso da camera
+  const [image, setImage] = useState(null); //Const para setar caminho da imagem
+  const [imageBase64, setImageBase64] = useState(null); //Const para setar imagem 64 bits
+  const [openCamera, setOpenCamera] = useState(false); //Estado de abertura de camera
+  const [respost, setRespost] = useState<string | null>(); //Const resposta para conferir
+  const [analyzing, setAnalyzing] = useState<boolean>(false); //Verificar estado da análise
   const cameraRef = useRef(null);
 
   if (!permission) {
     return <View />;
   }
 
+  //Verificar se tem acesso a câmera, se não tiver, retona
   if (!permission.granted) {
-    //retorna se não tiver permissao
     return (
       <View style={styles.container}>
         <Title title='Permissão' />
@@ -49,18 +49,86 @@ export default function ImiteAExpressao() {
               onPress={requestPermission}
             />
           </View>
-
         </View>
       </View>
     );
   }
 
+  //Funão para verificar os resultados recebidos da API.
+  const analyzeResult = (respost: Respost) => {
+   
+    if (respost) {
+      // Verifica se alguma propriedade tem valor "VERY_LIKELY"
+      if (respost.angerLikelihood === "VERY_LIKELY") {
+        return "raiva";
+      }
+      if (respost.joyLikelihood === "VERY_LIKELY") {
+        return "feliz";
+      }
+      if (respost.surpriseLikelihood === "VERY_LIKELY") {
+        return "surpreso";
+      }
+      if (respost.sorrowLikelihood === "VERY_LIKELY") {
+        return "triste";
+      }
+
+      // Verifica se alguma propriedade tem valor "LIKELY"
+      if (respost.angerLikelihood === "LIKELY") {
+        return "raiva";
+      }
+      if (respost.joyLikelihood === "LIKELY") {
+        return "feliz";
+      }
+      if (respost.surpriseLikelihood === "LIKELY") {
+        return "surpreso";
+      }
+      if (respost.sorrowLikelihood === "LIKELY") {
+        return "triste";
+      }
+
+      // Verifica se alguma propriedade tem valor 'PROBABLE", sozinho.
+      if (respost.angerLikelihood === "PROBABLE" &&
+        respost.joyLikelihood != "PROBABLE" &&
+        respost.surpriseLikelihood != "PROBABLE" &&
+        respost.sorrowLikelihood != "PROBABLE") {
+
+        return "raiva";
+      }
+      if (respost.joyLikelihood === "PROBABLE" &&
+        respost.angerLikelihood != "PROBABLE" &&
+        respost.surpriseLikelihood != "PROBABLE" &&
+        respost.sorrowLikelihood != "PROBABLE") {
+        return "feliz";
+      }
+      if (respost.surpriseLikelihood === "PROBABLE" &&
+        respost.joyLikelihood != "PROBABLE" &&
+        respost.angerLikelihood != "PROBABLE" &&
+        respost.sorrowLikelihood != "PROBABLE") {
+        return "surpreso";
+      }
+      if (respost.sorrowLikelihood === "PROBABLE" &&
+        respost.joyLikelihood != "PROBABLE" &&
+        respost.surpriseLikelihood != "PROBABLE" &&
+        respost.angerLikelihood != "PROBABLE") {
+        return "triste";
+      }
+      return null
+    }
+
+    // Se nenhum dos casos anteriores for verdadeiro, retorna null
+    return null;
+
+  }
+
+  //Função de enviar imagem para análise
   const analyzeImage = async () => {
     try {
       if (!imageBase64) {
         alert('Por favor, selecione a imagem primeiro');
         return;
       }
+
+      setAnalyzing(true);
 
       // carrrengado api
       const apiKey = 'AIzaSyCkMA9XocCebpM5JsG5-EHXesCK9FkvV8w';
@@ -75,7 +143,7 @@ export default function ImiteAExpressao() {
             "features": [
               {
                 "type": "FACE_DETECTION",
-                "maxResults": 7
+                "maxResults": 1
               }
             ]
           }
@@ -85,20 +153,26 @@ export default function ImiteAExpressao() {
       const apiResponse = await axios.post(apiURL, requestData);
       const parsedData = (apiResponse.data.responses[0].faceAnnotations);
       if (parsedData) {
-        console.log(parsedData)
-        setRespost(parsedData[0]);
+        const result: Respost = parsedData[0];
+        console.log(result);
+        setRespost(analyzeResult(result));
       }
-      
+
     } catch (error) {
       console.error('Erro ao enviar imagem: ', error);
       alert('Erro ao enviar imagem, tente novamente')
+    } finally {
+      setAnalyzing(false);
     }
   }
 
+
+  //Capura imagem
   const takePicture = async () => {
-    console.log('teste')
+
     if (cameraRef && cameraRef.current) {
       try {
+        //opções de configuração da imagem
         let option = {
           quality: 1,
           base64: true,
@@ -128,11 +202,11 @@ export default function ImiteAExpressao() {
       >
         <Camera
           style={styles.camera}
-          type={type}
+          type={CameraType.front}
           ref={cameraRef}
         >
           <TouchableOpacity onPress={takePicture} style={{ position: 'absolute', bottom: 50 }}>
-              <Entypo name="circle" size={70} color="black" />
+            <Entypo name="circle" size={70} color="black" />
           </TouchableOpacity>
         </Camera>
         <StatusBar hidden />
@@ -150,13 +224,13 @@ export default function ImiteAExpressao() {
             </TouchableOpacity>}
         </View>
         <View>
-            <View style={{flexDirection: 'row', marginTop: 20}}>
-              <ButtonGame disabled={!image}  style={styles.button} onPress={() => setImage(null)} text='Refazer'/>
-              <ButtonGame disabled={!image}  style={styles.button} onPress={analyzeImage} text='Verificar'/>
+          <View style={{ flexDirection: 'row', marginTop: 20 }}>
+            <ButtonGame disabled={!image} style={styles.button} onPress={() => setImage(null)} text='Refazer' />
+            <ButtonGame disabled={!image} style={styles.button} onPress={analyzeImage} text={!analyzing ? 'Verificar' : 'Analizando'} />
 
-            </View>
+          </View>
         </View>
-            <Text>{respost?.angerLikelihood}</Text>
+        <Text>{'Resultado: ' + respost}</Text>
       </View>
     </View>
   );
@@ -175,9 +249,7 @@ const styles = StyleSheet.create({
     marginTop: 50,
   },
   camera: {
-    width,
-    height,
-    aspectRatio: 1728 / 2304,
+    flex: 1,
     borderRadius: 60,
     position: 'absolute',
     top: 0,
@@ -185,12 +257,12 @@ const styles = StyleSheet.create({
   },
   picImage: {
     width: 300,
-    aspectRatio: 1728 / 2304,
+    aspectRatio:  1/1,
     transform: [{ scaleX: -1 }],
   },
   imagePic: {
     width: 350,
-    aspectRatio: 1728 / 2304,
+    aspectRatio: 1/1,
     borderRadius: 1,
     borderWidth: 1,
     alignItems: 'center',
